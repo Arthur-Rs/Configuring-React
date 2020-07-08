da64e422c3a1b560b970c043beedd31b560d8b50
Arthur-Rs <arthurreis074@gmail.com>
2020-07-08 20:10:13 -0300

    Added State in a component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function Header({children, title}) {
+/*
+* Components
+* Propriedades
+* Estado & Imutabilidade
+*/
+
+export default function Header({ title }) {
+  
+  const [projects, setProjects] = useState(['Desenvolviment', 'Classification'])
+
+  const HandleAddProject = () => {
+    setProjects([...projects, `Date: ${Date.now()}`])
+  }
+
   return (
     <header>
       <h1>{title}</h1>
-      {children}
+      <ul>
+        {projects.map((project) => <li key={project}>{project}</li>)}
+      </ul>
+      <button type="button" onClick={HandleAddProject}>Adicionar novo projeto</button>
     </header>
   )
 }
